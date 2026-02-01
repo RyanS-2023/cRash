62c537778f207f77523d769f031f3bc83de8bd2b
Tw1sT TeH N!NJA <Ninja-Tw1sT@users.noreply.github.com>
2026-01-31 23:38:16 -0600

    Use router-outlet as app shell

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SkolSlotComponent } from './skol-slot/skol-slot';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'skol-slot', pathMatch: 'full' },
+  { path: 'skol-slot', component: SkolSlotComponent },
+];
